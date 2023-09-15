36c31c69113b7b9a2310c1ff2c7541ea3557b915
suryatejaj97 <suryatejaj97@gmail.com>
2023-09-15 20:42:20 +0530

    signup and login functionality added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,28 +2,58 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const SignupPage = () => {
-  const [user, setUser] = React.useState({
+  const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
-  const [buttonDisabled, setButtonDisabled] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  
+  const router = useRouter();
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing" : "Signup"}</h1>
       <hr />
       <label htmlFor="username">Username</label>
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="username"
         type="text"
-        // value={user.username}
-        // onChange={(e) => setUser({ ...user, username: e.target.value })}
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
         placeholder="username"
       />
       <label htmlFor="email">Email</label>
@@ -31,8 +61,8 @@ const SignupPage = () => {
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="email"
         type="text"
-        // value={user.email}
-        // onChange={(e) => setUser({ ...user, email: e.target.value })}
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
       />
       <label htmlFor="password">Password</label>
@@ -40,8 +70,8 @@ const SignupPage = () => {
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="password"
         type="password"
-        // value={user.password}
-        // onChange={(e) => setUser({ ...user, password: e.target.value })}
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
       <button
